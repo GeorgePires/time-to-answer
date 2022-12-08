8b6b7bec03ebac750f7c93d42d0b15c6424031ba
GeorgePires <georgeasp.pi@gmail.com>
2022-12-07 21:03:42 -0300

    :recycle: refactor/ edit controller and views admins

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -5,5 +5,31 @@ module AdminsBackoffice
     def index
       @admins = Admin.all
     end
+
+    def edit
+      @admin = Admin.find(params[:id])
+    end
+
+    def update
+      @admin = Admin.find(params[:id])
+      if @admin.update_attributes(params[:admin_params])
+        flash[:success] = 'Admin was successfully updated'
+        redirect_to @admin
+      else
+        flash[:error] = 'Something went wrong'
+        render 'edit'
+      end
+    end
+
+    def destroy
+      @admin = Admin.find(params[:id])
+      if @admin.destroy
+        flash[:success] = 'Object was successfully deleted.'
+        redirect_to admins_backoffice_admins_path
+      else
+        flash[:error] = 'Something went wrong'
+        redirect_to admins_backoffice_admins_path
+      end
+    end
   end
 end
