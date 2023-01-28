09214a635b6e3cce768e835d90d44441fe29aa81
GeorgePires <georgeasp.pi@gmail.com>
2023-01-27 23:30:43 -0300

    :sparkles: feat/ list questions with kaminari

--- app/controllers/home/welcome_controller.rb
+++ app/controllers/home/welcome_controller.rb
@@ -3,7 +3,7 @@
 module Home
   class WelcomeController < HomeController
     def index
-      @questions = Question.includes(:answers)
+      @questions = Question.includes(:answers).page(params[:page]).per(4)
     end
   end
 end
