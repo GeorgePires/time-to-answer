89cada4dfaeab0ebb8f4716770997ed3d2d30e34
GeorgePires <georgeasp.pi@gmail.com>
2023-03-22 09:06:36 -0300

    :heavy_plus_sign: build: set migration and routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_17_031212) do
+ActiveRecord::Schema.define(version: 2023_03_21_185814) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,7 +57,6 @@ ActiveRecord::Schema.define(version: 2023_02_17_031212) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "user_name", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "first_name"
