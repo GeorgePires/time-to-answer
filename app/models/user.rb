163bfd93d0a80f936f4f622a902655b7ec4130ee
GeorgePires <georgeasp.pi@gmail.com>
2023-03-22 09:28:01 -0300

    :sparkles: feat/ set validates to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :first_name, :last_name, presence: true
+  validates :first_name, length: {minimum:3}
 
   def full_name
     "#{first_name} #{last_name}"
