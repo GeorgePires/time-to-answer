7610b7cfe2b27d4f96a9a727dc481337f8ea4946
GeorgePires <georgeasp.pi@gmail.com>
2023-04-13 21:53:52 -0300

    :sparkles: feat: add select genders in profile user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one :user_profile
+  has_one :user_profile, dependent: :destroy
   accepts_nested_attributes_for :user_profile, reject_if: :all_blank
 
   validates :first_name, :last_name, presence: true
--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 class UserProfile < ApplicationRecord
+  extend Enumerize
+
   belongs_to :user
+
+  enumerize :gender, in: %i[female male non_binary i_prefer_not_to_inform]
 end
